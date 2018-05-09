32697b435455c7fcffdb81d17d9119a8e265b85f
luapmg <luapmg@outlook.es>
2018-05-09 12:25:21 -0500

    subiendo archivos

---
+++ mysql.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  8 20:39:50 2018
+
+@author: luap
+"""
+
+import mysql.connector
+
+#cnx = mysql.connector.connect(user='paul', password='root',
+#                              host='127.0.0.1',
+#                              database='profeco_2016')
+#cnx.close()
